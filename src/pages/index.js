45d78711fd497e83438fb790376a5d89b904fc5a
ohill92 <oliver.hill.16@ucl.ac.uk>
2018-11-12 23:30:44 +0100

    commit four

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Link from 'gatsby-link'
 const IndexPage = () => (
   <div>
     <h1>Learn to code and design React apps</h1>
-    <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift/</p>
+    <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift</p>
     <Link to="/page-2/">Watch the video.</Link>
   </div>
 )
